f9d244230cc5c7ba60777180b6627dfc6a2f485d
Cristian Deleon <libaration@gmail.com>
2021-11-20 20:11:37 -0500

    add carousel for testimonials component

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -3,7 +3,16 @@ import DotLoader from 'react-spinners/DotLoader';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { useEffect, useState } from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 export default function Testimonials(props) {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   const [testimonials, setTestimonials] = useState(null);
   const getTestimonials = async () => {
     const docs = await getDocs(collection(db, 'testimonials'));
@@ -11,9 +20,9 @@ export default function Testimonials(props) {
   };
   const renderTestimonials = () => {
     console.log(testimonials);
-    return testimonials.map((testimonial) => {
+    return testimonials.map((testimonial, index) => {
       return (
-        <div className="text-1xl w-48 md:mt-10 lg:mt-20 max-w-xs max-h-xs z-40 overflow-hidden text-blue-800">
+        <div className="text-1xl text-blue-800 h-28 w-full overflow-hidden md:mt-16">
           "{testimonial.data().testimonial}"
           <br />
           <span className="text-xs">- {testimonial.data().author}</span>
@@ -25,8 +34,23 @@ export default function Testimonials(props) {
     getTestimonials();
   }, []);
   return (
-    <div className="flex text-center justify-center align-center">
-      {testimonials ? renderTestimonials() : <DotLoader />}
+    <div className="flex text-center justify-center align-center text-center">
+      {testimonials ? (
+        <Carousel
+          className="overflow-hidden"
+          showStatus={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          centerMode={true}
+          showIndicators={false}
+          showThumbs={false}
+          centerSlidePercentage={100}
+        >
+          {renderTestimonials()}
+        </Carousel>
+      ) : (
+        <DotLoader />
+      )}
     </div>
   );
 }
